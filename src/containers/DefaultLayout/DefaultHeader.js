4e36c8098fe7b56c394a2fe07024d7b351961a71
droza0 <droza0@users.noreply.github.com>
2019-11-03 13:24:00 -0500

    added logo

--- src/containers/DefaultLayout/DefaultHeader.js
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -4,7 +4,7 @@ import { Badge, UncontrolledDropdown, DropdownItem, DropdownMenu, DropdownToggle
 import PropTypes from 'prop-types';
 
 import { AppAsideToggler, AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
-import logo from '../../assets/img/brand/logo.svg'
+import logo from '../../assets/img/brand/solidarity.png'
 import sygnet from '../../assets/img/brand/sygnet.svg'
 
 const propTypes = {
