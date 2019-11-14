b3f79d18b34581811fb2d0d6bcf9ecc30fe89b00
droza0 <droza0@users.noreply.github.com>
2019-11-14 17:15:34 -0500

    added user profile

--- src/views/Users/User.js
+++ src/views/Users/User.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
-import { Card, CardBody, CardHeader, Col, Row, Table } from 'reactstrap';
+import { Card, CardImg, CardBody, CardHeader, Col, Row, Table } from 'reactstrap';
 
 import usersData from './UsersData'
+
+const userData = {
+  authId: '',
+  email: 'fire@gmail.com',
+  firstName: 'John',
+  lastName: 'Smith',
+  sensorId: 1,
+  pressure: 80.5,
+  humidity: 80,
+  temperature: 90,
+  latitude: 40.6643,
+  longitude: -73.9385
+}
 
 class User extends Component {
   
@@ -18,21 +31,42 @@ class User extends Component {
               <CardHeader>
                 <strong><i className="icon-info pr-1"></i>User id: {this.props.match.params.id}</strong>
               </CardHeader>
+              <CardImg top width="50%" src="https://i.imgur.com/uRHecNh.png" alt="Card image cap" />
               <CardBody>
                   <Table responsive striped hover>
                     <tbody>
-                      {
-                        usersData.map((item) => {
-                          console.log(item)
-                          return (
-                            <tr>
-                              <td>{`${item.firstName} ${item.lastName}:`}</td>
-                              <td><strong>{item.email}</strong></td>
-                            </tr>
-                          )
-                        })
-                        
-                      }
+                      <tr>
+                        <td>{`Name:`}</td>
+                        <td><strong>{`${userData.firstName} ${userData.lastName}`}</strong></td>
+                      </tr>
+                      <tr>
+                        <td>{`Email:`}</td>
+                        <td><strong>{userData.email}</strong></td>
+                      </tr>
+                      <tr>
+                        <td>{`Sensor Id:`}</td>
+                        <td><strong>{userData.sensorId}</strong></td>
+                      </tr>
+                      <tr>
+                        <td>{`Pressure`}</td>
+                        <td><strong>{userData.pressure}</strong></td>
+                      </tr>
+                      <tr>
+                        <td>{`Temperature`}</td>
+                        <td><strong>{userData.temperature}</strong></td>
+                      </tr>
+                      <tr>
+                        <td>{`Humidity`}</td>
+                        <td><strong>{userData.humidity}</strong></td>
+                      </tr>
+                      <tr>
+                        <td>{`Latitude`}</td>
+                        <td><strong>{userData.latitude}</strong></td>
+                      </tr>
+                      <tr>
+                        <td>{`Longitude`}</td>
+                        <td><strong>{userData.longitude}</strong></td>
+                      </tr>
                     </tbody>
                   </Table>
               </CardBody>
