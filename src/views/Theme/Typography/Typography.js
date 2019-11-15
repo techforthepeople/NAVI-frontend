c1c0c9a690e6c767bdbab28a92162464d1d9919f
Greashun <vncntts@gmail.com>
2019-11-14 19:40:41 -0500

    fixed input size

--- src/views/Theme/Typography/Typography.js
+++ src/views/Theme/Typography/Typography.js
@@ -59,7 +59,7 @@ class Forms extends Component {
               <CardBody>
                 <Form action="" method="post" encType="multipart/form-data" className="form-horizontal">
                   <FormGroup row>
-                    <Col md="2">
+                    <Col md="7">
                       <Label htmlFor="exampleInputName2" className="pr-1">First Name</Label>
                       <Input type="text" id="exampleInputName2" placeholder="Jane" required />
             
@@ -69,26 +69,21 @@ class Forms extends Component {
                   </FormGroup>
                  
                   <FormGroup row>
-                    <Col md='3'> 
+                    <Col md='6'> 
                       <Label htmlFor='DOB'>Date of Birth</Label>
-                    </Col>
-                    <Col>
                       <Input type='dob' id='dob-input' name='dob-input' placeholder='mm/dd/yyyy'></Input>
                       <FormText>Enter Date of Birth</FormText>
-                    </Col>
-                  </FormGroup>
-                  
-                  <FormGroup row>
-                    <Col md='3'>
+
                       <Label>Sex</Label>
-                    </Col>
-                    <Col>
                       <Input type='sex-gender' id='sex-input' name='sex-input' placeholder='M / F / Prefer not to say'/>
                     </Col>
+                    <Col>
+
+                    </Col>
                   </FormGroup>
                   
                   <FormGroup row>
-                    <Col md='3'> 
+                    <Col md='10'> 
                       <Label>Street</Label>
                       <Input type='street-address' id='street-input' name='street-input'>
                       </Input>
@@ -125,62 +120,8 @@ class Forms extends Component {
                 <Button type="reset" size="sm" color="danger"><i className="fa fa-ban"></i> Reset</Button>
               </CardFooter>
             </Card>
-            <Card>
-              <CardHeader>
-                <strong>Inline</strong> Form
-              </CardHeader>
-              <CardBody>
-                <Form action="" method="post" inline>
-                  <FormGroup className="pr-1">
-                    <Label htmlFor="exampleInputName2" className="pr-1">Name</Label>
-                    <Input type="text" id="exampleInputName2" placeholder="Jane Doe" required />
-                  </FormGroup>
-                  <FormGroup className="pr-1">
-                    <Label htmlFor="exampleInputEmail2" className="pr-1">Email</Label>
-                    <Input type="email" id="exampleInputEmail2" placeholder="jane.doe@example.com" required />
-                  </FormGroup>
-                </Form>
-              </CardBody>
-              <CardFooter>
-                <Button type="submit" size="sm" color="primary"><i className="fa fa-dot-circle-o"></i> Submit</Button>
-                <Button type="reset" size="sm" color="danger"><i className="fa fa-ban"></i> Reset</Button>
-              </CardFooter>
-            </Card>
           </Col>
-          <Col xs="12" md="6">
-            <Card>
-              <CardHeader>
-                <strong>Horizontal</strong> Form
-              </CardHeader>
-              <CardBody>
-                <Form action="" method="post" className="form-horizontal">
-                  <FormGroup row>
-                    <Col md="3">
-                      <Label htmlFor="hf-email">Email</Label>
-                    </Col>
-                    <Col xs="12" md="9">
-                      <Input type="email" id="hf-email" name="hf-email" placeholder="Enter Email..." autoComplete="email" />
-                      <FormText className="help-block">Please enter your email</FormText>
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Col md="3">
-                      <Label htmlFor="hf-password">Password</Label>
-                    </Col>
-                    <Col xs="12" md="9">
-                      <Input type="password" id="hf-password" name="hf-password" placeholder="Enter Password..." autoComplete="current-password" />
-                      <FormText className="help-block">Please enter your password</FormText>
-                    </Col>
-                  </FormGroup>
-                </Form>
-              </CardBody>
-              <CardFooter>
-                <Button type="submit" size="sm" color="primary"><i className="fa fa-dot-circle-o"></i> Submit</Button>
-                <Button type="reset" size="sm" color="danger"><i className="fa fa-ban"></i> Reset</Button>
-              </CardFooter>
-            </Card>
-  
-          </Col>
+
         </Row>
        </div>
     );
